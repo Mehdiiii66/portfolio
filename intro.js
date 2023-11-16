105e13dc900e5f8cbadf83e210b5a94a32298d1d
Mehdiiii66 <114617033+Mehdiiii66@users.noreply.github.com>
2023-11-16 19:32:06 +0100

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,12 @@
+// const slide = document.querySelector(".slide")
+
+// window.addEventListener('scroll', () => {
+
+//     const {scrollTop, clientHeight} = document.documentElement;
+
+//     const topElementToTopViewPort = slide.getBoundingClientRect().top;
+
+//     if(scrollTop > (scrollTop + topElementToTopViewPort).toFixed() - clientHeight * 0.95) {
+//         slide.classList.add('active')
+//     }
+// })
