019e295dc13bf1d7075a39d92c715d814c140253
Mehdiiii66 <114617033+Mehdiiii66@users.noreply.github.com>
2023-11-18 19:51:30 +0100

    Add files via upload

--- intro.js
+++ intro.js
@@ -75,3 +75,82 @@ window.addEventListener('scroll', () => {
         c.classList.add('active')
     }
 })
+
+
+const naimUn = document.querySelector(".naim-un")
+
+window.addEventListener('scroll', () => {
+
+    const {scrollTop, clientHeight} = document.documentElement;
+
+    const topElementToTopViewPort = naimUn.getBoundingClientRect().top;
+
+    if(scrollTop > (scrollTop + topElementToTopViewPort).toFixed() - clientHeight * 0.85) {
+        naimUn.classList.add('active')
+    }
+})
+
+const naimDeux = document.querySelector(".naim-deux")
+
+window.addEventListener('scroll', () => {
+
+    const {scrollTop, clientHeight} = document.documentElement;
+
+    const topElementToTopViewPort = naimDeux.getBoundingClientRect().top;
+
+    if(scrollTop > (scrollTop + topElementToTopViewPort).toFixed() - clientHeight * 0.85) {
+        naimDeux.classList.add('active')
+    }
+})
+
+const naimTrois = document.querySelector(".naim-trois")
+
+window.addEventListener('scroll', () => {
+
+    const {scrollTop, clientHeight} = document.documentElement;
+
+    const topElementToTopViewPort = naimTrois.getBoundingClientRect().top;
+
+    if(scrollTop > (scrollTop + topElementToTopViewPort).toFixed() - clientHeight * 0.85) {
+        naimTrois.classList.add('active')
+    }
+})
+
+const naimPython = document.querySelector(".naim-python")
+
+window.addEventListener('scroll', () => {
+
+    const {scrollTop, clientHeight} = document.documentElement;
+
+    const topElementToTopViewPort = naimPython.getBoundingClientRect().top;
+
+    if(scrollTop > (scrollTop + topElementToTopViewPort).toFixed() - clientHeight * 0.80) {
+        naimPython.classList.add('active')
+    }
+})
+
+const naimHtml = document.querySelector(".naim-html")
+
+window.addEventListener('scroll', () => {
+
+    const {scrollTop, clientHeight} = document.documentElement;
+
+    const topElementToTopViewPort = naimHtml.getBoundingClientRect().top;
+
+    if(scrollTop > (scrollTop + topElementToTopViewPort).toFixed() - clientHeight * 0.80) {
+        naimHtml.classList.add('active')
+    }
+})
+
+const naimC = document.querySelector(".naim-c")
+
+window.addEventListener('scroll', () => {
+
+    const {scrollTop, clientHeight} = document.documentElement;
+
+    const topElementToTopViewPort = naimC.getBoundingClientRect().top;
+
+    if(scrollTop > (scrollTop + topElementToTopViewPort).toFixed() - clientHeight * 0.80) {
+        naimC.classList.add('active')
+    }
+})
